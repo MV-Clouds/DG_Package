e4907b26d563ab374e006768b44bb2d9d333e2ec
KevinSuvagiya01 <kevin.s@mvclouds.com>
2024-08-21 22:01:30 +0530

    with resolving conflicts 21-08-2024 10:01 PM

--- force-app/main/default/lwc/customCombobox/customCombobox.js
+++ force-app/main/default/lwc/customCombobox/customCombobox.js
@@ -331,18 +331,6 @@ export default class CustomCombobox extends LightningElement {
     handleSearch(event){
         try {
             var searchValue = (event.target.value).toLowerCase();
-            this.setOptionAfterSearch(searchValue);
-        } catch (error) {
-            errorDebugger('CustomCombobox', 'handleSearch', error, 'warn');
-        }
-    }
-
-    /**
-     * Method to set option based search value.
-     * @param {*} searchValue 
-     */
-    setOptionAfterSearch(searchValue){
-        try {
             if(searchValue === null || searchValue.trim() === '' || searchValue === undefined){
                 this.displayOptions = this.allOptions;
             }
@@ -358,7 +346,7 @@ export default class CustomCombobox extends LightningElement {
             // sort After each Search
             this.sortDisplayItems();
         } catch (error) {
-            errorDebugger('CustomCombobox', 'setOptionAfterSearch', error, 'warn');
+            errorDebugger('CustomCombobox', 'handleSearch', error, 'warn');
         }
     }
 
@@ -646,9 +634,7 @@ export default class CustomCombobox extends LightningElement {
             if(this.searchable){
                 const searchInput = this.template.querySelector('[data-id="search-input"]');
                 searchInput && (searchInput.value = '');
-                this.setOptionAfterSearch(null);
             }
-
         } catch (error) {
             errorDebugger('CustomCombobox', 'clearSearch', error, 'warn');
         }
@@ -665,9 +651,9 @@ export default class CustomCombobox extends LightningElement {
             // if isInvalid is "TRUE" --> Show Error Border...
             if(isInvalid){
                 this.template.querySelector('.slds-combobox__input')
-                .style = `  var(--invalidInputBackground, background-color: rgb(255, 255, 255));
-                            var(--invalidInputBorderColor, border-color: rgb(238 72 65));
-                            var(--invalidInputShadow, box-shadow: rgb(243 82 76) 0px 0px 1px 1px);
+                .style = `  background-color: rgb(255, 255, 255);
+                            border-color: rgb(238 72 65);
+                            box-shadow: rgb(243 82 76) 0px 0px 1px 1px;
                 `;
             }
             // else Remove Error Border...
@@ -676,6 +662,7 @@ export default class CustomCombobox extends LightningElement {
             }
         } catch (error) {
             errorDebugger('CustomCombobox', 'isInvalidInput', error, 'warn');
+            
         }
     }
 
--- force-app/main/default/lwc/documentLoader/documentLoader.js
+++ force-app/main/default/lwc/documentLoader/documentLoader.js
@@ -4,11 +4,11 @@ export default class DocumentLoader extends LightningElement {
 
 	_fullSize;			// In case of need to set CSS "Position : fixed"...
     @api get fullSize(){ return this._fullSize}
-    set fullSize(value){ this._fullSize = value === true || value === "true" ? true : false}
+    set fullSize(value){ this._fullSize = value}
 
     _noBackdrop;		// In Case You Don't want to show backdrop effect...
     @api get noBackdrop(){ return this._noBackdrop}
-    set noBackdrop(value){ this._noBackdrop = value === true || value === "true" ? true : false}
+    set noBackdrop(value){ this._noBackdrop = value}
 
     _customScale;		// IF You want to set custom Size of a Loader...
     @api get customScale(){ return this._customScale}
@@ -16,7 +16,7 @@ export default class DocumentLoader extends LightningElement {
 
     _fixTop;			// To Resolve the top position offset issue....
     @api get fixTop(){ return this._fixTop}
-    set fixTop(value){ this._fixTop = value === true || value === "true" ? true : false}
+    set fixTop(value){ this._fixTop = value}
 
 	scaleOutTime = 250;
 	isInitial = true;
@@ -72,10 +72,10 @@ export default class DocumentLoader extends LightningElement {
 	// ** === Custom Styling / CSS Method - START - =====
 	get loadStyle(){
 		var style = `--scaleOutTime : ${this.scaleOutTime}ms;`;
-		style += this.fullSize === true ? `position : fixed !important;` : '';
-		style += this.noBackdrop === true ? `background : transparent !important; backdrop-filter : none !important;` : '';
+		style += this.fullSize == "true" ? `position : fixed !important;` : '';
+		style += this.noneBackdrop == "true" ? `background : transparent !important; backdrop-filter : none !important;` : '';
 		style += this.customScale ? `--documentScale : ${this.customScale};` : '';
-		style += this.fixTop === true ? '--docTop: 0rem;' : '';
+		style += this.fixTop == 'true' ? '--docTop: 0rem;' : '';
 		return style;
 	}
 	// === Custom Styling / CSS Method - END - =====
